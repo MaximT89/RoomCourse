67722f5c3f464aadc0a02d9d7a7de974c34bbd84
Максим Толстобров <maximah1989@yandex.ru>
2021-08-03 08:28:21 +0300

    Create lesson

--- app/src/main/java/com/example/roomcourse/MainActivity.java
+++ app/src/main/java/com/example/roomcourse/MainActivity.java
@@ -10,7 +10,6 @@ import com.example.roomcourse.databinding.ActivityMainBinding;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private AppDatabase db;
     private ActivityMainBinding binding;
 
     @Override
@@ -18,27 +17,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(LayoutInflater.from(this));
         setContentView(binding.getRoot());
-        initView();
 
-        binding.btnResult.setOnClickListener(v -> {
-            StringBuilder stringBuilder = new StringBuilder();
 
-            User user = new User();
-            user.mUserName = binding.editName.getText().toString();
-            user.mUserAge = Integer.parseInt(binding.editAge.getText().toString());
-            user.mUserCity = binding.editCity.getText().toString();
 
-            List<User> userList = db.userDao().insertAndGetAllUsers(user);
-
-            for (int i = 0; i < userList.size(); i++) {
-                stringBuilder.append(userList.get(i)).append("\n\n");
-            }
-
-            binding.textResult.setText(stringBuilder);
-        });
     }
 
-    private void initView() {
-        db = App.getInstance().getDatabase();
-    }
+
 }
